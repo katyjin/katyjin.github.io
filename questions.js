31a55a0ba43ef31094a975c8335a1fcb1f45145e
katyjin <153544602+katyjin@users.noreply.github.com>
2023-12-11 15:35:48 -0500

    MUN

---
+++ questions.js
@@ -0,0 +1,56 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "1.	What does MUN stand for?",
+    answer: "Model United Nations",
+    options: [
+      "Military Understanding Negotiation",
+      "Model Union Nation",
+      "Model United Nations",
+      "Mock Ultimate Nations"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many permanent members does the UN have?",
+    answer: "5",
+    options: [
+      "4",
+      "5",
+      "6",
+      "7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the pronoun that should be used in the MUN (double choice)?",
+    answer: "Third person or Countries’ name",
+    options: [
+      "First person",
+      "Second person and Countries' name",
+      "Third person or Countries’ name",
+        "Third person"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A motion is raised when",
+    answer: "To move to a moderated caucus",
+    options: [
+      "To pass something to the chair",
+      "To move to a moderated caucus",
+      "To request something from the chair",
+      "To move to an unmoderated caucus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "5. After which major war was the United Nations Charter established?",
+    answer: "World War I",
+    options: [
+      "Civil war",
+        "World War I",
+        "World War II",
+      "Vietnam War"]}]
+    
---
+++ yourlevel.js
@@ -0,0 +1,12 @@
+function yourlevel{
+var x=(prompt("please input your answer:"))
+   if(x==yes){
+    alert("tutorial")
+}
+   elseif(x==no){
+    alert("advanced")
+}
+   else{
+    alert("please submit one of the given option")
+}
+}
